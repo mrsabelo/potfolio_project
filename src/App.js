f024d9764e6a1b5a6535d9481a1d843c46544aac
mrsabelo <mrsabelovilakati@gmail.com>
2021-11-25 16:16:48 +0200

    Header Completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,40 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css"
 import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Particles from 'react-particles-js';
 
 
 function App() {
   return (
-    <div className="App">
+    <>
       <Navbar />
+      <Particles
+        className="particles-canvas"
+          params={{
+          particles: {
+              number: {
+                value: 50,
+                density: {
+                  enable: true,
+                  value_area:900
+                }
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  with: 20,
+                  color: "#f9ab00"
+                }
+              },
+             
+            }
+        }}
+      />
       
+      <Header />
       
-    </div>
+    </>
   );
 }
 
